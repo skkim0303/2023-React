f69019127b059ba44cac54bb8236f764967d6894
skkim0303 <76196599+skkim0303@users.noreply.github.com>
2023-02-19 23:46:57 +0900

    Delete GuGuDan-Pack directory

--- GuGuDan-Pack/client.jsx
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-const GuGuDan = require('./GuGuDan'); // GuGuDan 코드 불러오기
-
-// ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
-
-// 18버전으로 try
-ReactDom.createRoot(document.querySelector('#root')).render(<GuGuDan />);
